a7cfab47b7842a4b96507399c7a49e4aefc3d3c9
kungyu <hismayfly@gmail.com>
2021-01-08 18:09:56 +0900

    [KG : add] formatted print

--- hello.rs
+++ hello.rs
@@ -1,4 +1,13 @@
 fn main() {
     println!("Hello World");
     println!("I'm a Rustacean!");
+    // using comment
+    // automatic comment
+    /*
+     * multi-line comment
+     * automatic star
+     * with every enter
+     */
+    let x = 5 + /*in-line comment*/ 5;
+    println!("Is 'x' 10 or 100? x= {}", x);
 }
